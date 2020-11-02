5f95469cab78b7d6b165b26afe0d772a39ff57ba
vornsami <sami.vornanen@helsinki.fi>
2020-11-02 15:03:35 +0200

    tehtävä 15

---
+++ viikko1/tehtavat14-16/NhlStatistics1/src/main/java/ohtuesimerkki/Reader.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ohtuesimerkki;
+
+import java.util.List;
+
+/**
+ *
+ * @author Sami
+ */
+public interface Reader {
+    List<Player> getPlayers();
+}
